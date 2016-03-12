e4f7d6e46b320abd7b9c5c78fcf98d23e830018a
Frank Matranga <thefrankmatranga@gmail.com>
2016-03-11 19:00:30 -0500

    Calendar

--- client/main.js
+++ client/main.js
@@ -8,6 +8,8 @@ updateDate = function() {
   var newDate = moment(Session.get('date')).format("ddd, MMM Do");
   document.title = "Lister - " + newDate;
   history.pushState({}, "Lister - " + newDate, moment(Session.get('date')).format("YYYY-MM-DD"));
+  $('#calendar').fullCalendar('refetchEvents');
+  $('#calendar').fullCalendar( 'gotoDate', moment(Session.get('date')));
 }
 
 Template.day.helpers({
@@ -126,6 +128,7 @@ Template.addAssignment.events({
     Meteor.call("addAssignment", date, subject, description);
 
     event.target.description.value = "";
+    $('#calendar').fullCalendar('refetchEvents');
   }
 });
 
@@ -138,9 +141,59 @@ Template.assignment.helpers({
 Template.assignment.events({
   "click .toggle-completed": function(event, template) {
     Meteor.call("setCompleted", this._id, !this.completed);
+    $('#calendar').fullCalendar('refetchEvents');
   },
   "click .remove-assignment": function(event, template) {
-    if (confirm("Remove this assignment?"))
+    if (confirm("Remove this assignment?")){
       Meteor.call("deleteAssignment", this._id);
+      $('#calendar').fullCalendar('refetchEvents');
+    }
+  }
+});
+
+
+Template.calendar.helpers({
+  options: function() {
+    return {
+      id: "calendar",
+      weekends: false,
+      defaultView: 'month',
+      dayClick: function(date, jsEvent, view) {
+        // I don't know why it's one day behind
+        Session.set('date', date.add(1, 'days').toDate());
+        updateDate();
+      },
+      events: function(start, end, timezone, callback) {
+        var events = [];
+        var days = {};
+
+        Assignments.find({
+          owner: Meteor.userId(),
+          date: {$gte: start.toDate(), $lte: end.toDate()}
+        }).forEach(function(a) {
+          console.log(a);
+          if(!days[moment(a.date)])
+            days[moment(a.date)] = {};
+
+          if(!days[moment(a.date)][a.subject])
+            days[moment(a.date)][a.subject] = [];
+
+          days[moment(a.date)][a.subject].push(a);
+        });
+
+        for (var date in days) {
+          for (var sub in days[date]) {
+            events.push({
+              start: date,
+              allDay: true,
+              title: days[date][sub].length+" "+subjects[sub]
+            });
+          }
+        }
+
+        callback(events);
+        return events;
+      }
+    };
   }
 });
